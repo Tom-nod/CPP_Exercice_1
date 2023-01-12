f28218b36fe88068a8707c8c75eb0bfd92ea88c8
Thomas NODA <tomasnoda8@gmail.com>
2023-01-12 15:02:59 +0100

    test commit

--- TP1/helloworld.cpp
+++ TP1/helloworld.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 
+int add(int a, int b) {
+  return a + b;
+}
+
+void add_to(int& a, int b) {
+  a += b;
+}
+
 int main() {
     std::string name;
     std::cin >> name;
     std::cout << "Hello " << name << "!" << std::endl;
+
+    const int x{10};
+    int y = add(x, x);
+    add_to(y, 22);
+    std::cout << y << std::endl;
     return 0;
 }
