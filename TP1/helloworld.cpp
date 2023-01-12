a523eceb5998c705c54b804b0651cad3003795fe
Thomas NODA <thomas.noda@edu.univ-mlv.fr>
2023-01-12 14:37:19 +0100

    "change cin"

--- TP1/helloworld.cpp
+++ TP1/helloworld.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    int var;
+    std::cin >> var;
+    std::cout << "Hello World!" << var << std::endl;
     return 0;
 }
