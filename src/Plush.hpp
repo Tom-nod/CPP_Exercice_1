518fcda2fea467f41295c7609c0a0b8f620377ef
Thomas NODA <tomasnoda8@gmail.com>
2023-01-26 14:23:24 +0100

    Changes

--- src/Plush.hpp
+++ src/Plush.hpp
@@ -1,1 +1,25 @@
 #pragma once
+
+class Plush {
+public:
+    Plush(){
+        _cost = 10;
+    }
+
+    Plush(const int costInit)
+        : _cost {costInit}
+    {
+        _cost = costInit;
+    }
+
+    int get_cost() const {
+        return _cost;
+    }
+
+    void set_cost(int cost){
+        _cost = cost;
+    }
+    
+private:
+    int         _cost ;
+};
--- src/PlushStore.hpp
+++ src/PlushStore.hpp
@@ -1,1 +1,62 @@
 #pragma once
+
+#include <iostream>
+#include <cmath>
+
+class PlushStore {
+    public:
+    PlushStore(const std::string& name)
+        :_name(name){
+            _name = name;
+            _wealth_amount = 0;
+            _stock_size = 0;
+            _debt_amount = 0;
+            _experience = 0;
+        }
+    
+    std::string get_name() const{
+        return _name;
+    }
+
+    int get_wealth_amount() const{
+        return _wealth_amount;
+    }
+
+    int get_stock_size() const {
+        return _stock_size;
+    }
+
+    int get_debt_amount() const {
+        return _debt_amount;
+    }
+
+    void loan(int amount) {
+        _wealth_amount += amount;
+        _debt_amount += amount * 1.1;
+    }
+
+    int make_plush(int cost) {
+        int value;
+        if(_wealth_amount != 0){
+            ++_stock_size;
+            ++_experience;
+            value = abs(_wealth_amount - cost);
+            _wealth_amount -= value;
+            if(_wealth_amount < 0){
+                _wealth_amount = 0;
+            }
+        }
+        return value;
+    }
+
+    int get_experience() const {
+        return _experience;
+    }
+
+    private:
+    std::string     _name;
+    int             _wealth_amount;
+    int             _stock_size;
+    int             _debt_amount;
+    int             _experience;
+};
