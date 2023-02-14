705e6f7401541b7d384a60734f6e1ec4b54422b0
Thomas NODA <tomasnoda8@gmail.com>
2023-02-14 10:32:52 +0100

    trace exo 9

--- src/Board.hpp
+++ src/Board.hpp
@@ -1,1 +1,14 @@
 #pragma once
+
+#include <vector>
+
+#include "Monster.hpp"
+#include "Spell.hpp"
+#include "Trap.hpp"
+
+class Board {
+    public:
+
+    private:
+        std::vector<Monster>            _monster;
+};
