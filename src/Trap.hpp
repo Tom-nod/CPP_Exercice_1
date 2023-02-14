401e96dcab4b12d184cefa53a5b72909ca75bb6b
Thomas NODA <tomasnoda8@gmail.com>
2023-02-14 10:09:41 +0100

    exo 7 done

--- src/Trap.hpp
+++ src/Trap.hpp
@@ -1,1 +1,36 @@
 #pragma once
+
+#include "Card.hpp"
+
+enum class TrapType {Normal, Continuous, Counter};
+
+const std::string to_string(const TrapType spell) {
+    switch (spell)
+    {
+    case TrapType::Normal:
+        return "Normal";
+        break;
+    case TrapType::Continuous:
+        return "Continuous";
+        break;
+    case TrapType::Counter:
+        return "Counter";
+    }
+    return "";
+}
+
+class Trap : public Card {
+    public:
+        Trap(const std::string& id, const std::string& name, TrapType trap)
+        : Card(id, CardType::Trap), _trap(trap)
+        {
+            set_name(name);
+        }
+
+        TrapType get_trap_type() const {
+            return _trap;
+        }
+
+    private:
+        TrapType           _trap;
+};
