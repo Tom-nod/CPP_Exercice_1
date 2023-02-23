16cb603d04596e4a8d5f09ad7470f19bc2e451de
Thomas NODA <tomasnoda8@gmail.com>
2023-02-23 09:42:04 +0100

    exo 4 + 5 done

--- src/ArrayNode.hpp
+++ src/ArrayNode.hpp
@@ -1,1 +1,19 @@
 #pragma once
+
+#include <vector>
+
+#include "Node.hpp"
+
+class ArrayNode : public Node {
+    public:
+    ArrayNode(){
+
+    }
+
+    const std::string print() const override {
+        return "[]";
+    }
+
+    private:
+    std::vector<Node>        _array;
+};
--- src/ObjectNode.hpp
+++ src/ObjectNode.hpp
@@ -1,1 +1,17 @@
 #pragma once
+
+#include "Node.hpp"
+
+class ObjectNode : public Node {
+    public:
+    ObjectNode(){
+
+    }
+
+    const std::string print() const override {
+        return "{}";
+    }
+
+    private:
+
+};
