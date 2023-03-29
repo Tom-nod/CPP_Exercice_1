b53f03da47a4531dae834cf8de4c80f797236ff0
Thomas NODA <tomasnoda8@gmail.com>
2023-03-29 11:20:44 +0200

    test 0102 done

--- src/image.hpp
+++ src/image.hpp
@@ -18,12 +18,21 @@ class Image {
 
         Image<P, W, H>(const P& pixel)
         {
-            for(int i = 0; i < W; ++i){
-                for(int j = 0; j < H; ++j){
+            for(size_t i = 0; i < W; ++i){
+                for(size_t j = 0; j < H; ++j){
                     _array[i][j] = pixel;
                 }
             }
         }
+
+        const P& operator()(size_t i, size_t j) const {
+            return _array[j][i];
+        }
+
+        P& operator()(size_t i, size_t j) {
+            return _array[j][i];
+        }
+
     private:
     std::array<std::array<P, W>, H> _array;
 };
