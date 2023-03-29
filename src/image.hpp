2f789c5362cec32a13ba3407ef387146b340baa4
Thomas NODA <tomasnoda8@gmail.com>
2023-03-29 11:12:26 +0200

    test 0101 done

--- src/image.hpp
+++ src/image.hpp
@@ -11,3 +11,19 @@
 #include <array>
 #include <iostream>
 
+template <typename P, size_t W, size_t H>
+class Image {
+    public:
+        Image<P, W, H>() = default;
+
+        Image<P, W, H>(const P& pixel)
+        {
+            for(int i = 0; i < W; ++i){
+                for(int j = 0; j < H; ++j){
+                    _array[i][j] = pixel;
+                }
+            }
+        }
+    private:
+    std::array<std::array<P, W>, H> _array;
+};
