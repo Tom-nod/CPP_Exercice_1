9e322a9057c50f319780ca791366f51dabbeb32f
Thomas NODA <tomasnoda8@gmail.com>
2023-04-07 15:03:29 +0200

    test 12 error

--- src/ex1.cpp
+++ src/ex1.cpp
@@ -1,1 +1,20 @@
 #include "ex1.hpp"
+
+#include "../lib/concatenate.hpp"
+
+void pairwise_concatenate(std::list<std::list<int>>& list1, std::list<std::list<int>>&& list2)
+{
+    auto iter = list2.begin();
+    for(auto list : list1){
+        concatenate(list, std::move(*iter));
+        iter++;
+    }
+}
+
+void pairwise_concatenate(std::list<std::list<int>>& list1, const std::list<std::list<int>>& list2){
+    auto iter = list2.begin();
+    for(auto list : list1){
+        concatenate(list, *iter);
+        iter++;
+    }
+}
--- src/ex1.hpp
+++ src/ex1.hpp
@@ -1,1 +1,5 @@
 #pragma once
+#include <list>
+
+void pairwise_concatenate(std::list<std::list<int>>& list1, std::list<std::list<int>>&& list2);
+void pairwise_concatenate(std::list<int>& list1, const std::list<int>& list2);
